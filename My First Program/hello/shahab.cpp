aee22974061db657b78461ae526bf727cb1db6d8
Muhmmad Shahab <shahabofficialkhan@gmail.com>
2025-11-28 23:53:57 +0500

    Add files via upload

---
+++ My First Program/hello/shahab.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cout << "this is me muhammad shahab " << std::endl;
+    cin >> a;
+    int b;
+    cin >>b;
+    int c=a+b;
+    cout <<"this is the addition of two number "<< std::endl;
+   cout <<"the sum of two number is : "<< c<< std::endl;
+        return 0;
+}
