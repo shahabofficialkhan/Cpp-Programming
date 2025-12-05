29aa69d95341997147683550a4550eb2b87fefc5
Muhmmad Shahab <shahabofficialkhan@gmail.com>
2025-12-05 23:41:38 +0500

    Gussing Game

---
+++ games/Gussinggame.cpp
@@ -0,0 +1,28 @@
+   #include <iostream>
+   #include  <iomanip>
+   using namespace std;
+          int selectionfunc(){
+                    switch (level)
+                    case 1 : {
+                              gussingnumber=1+%ran
+                    }
+
+          }
+
+          int levelfunc(){
+                     int level , gussingnumber ;
+          cout << "1) Click one for Basic level";
+          cout << "2) Click two for medium level";
+          cout << "3) Click three for advanced level";
+          cin >> level ;
+          return 0;
+          }
+
+   int main () {
+          
+          levelfunc (gussingnumber);
+
+
+
+           return 0;
+   }      
