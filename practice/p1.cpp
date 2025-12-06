42c65ea3c3dd92ee72ff50b14a82a7988caab8e2
Muhmmad Shahab <shahabofficialkhan@gmail.com>
2025-12-06 23:48:36 +0500

    practice

---
+++ practice/p1.cpp
@@ -0,0 +1,24 @@
+   #include <iostream>
+   using namespace std;
+      int main () {
+        //   int array[3]={}
+        //             // cout << "enter number";
+        //             // cin >> array[x];
+        //             int* p=array;
+
+        //             cout <<endl<< *p
+        //             cout <<endl<< *++p;     
+          // }
+
+        //   int sha[4];
+        //   int x=0;
+        //   while (x<4)
+        //   {
+        //     cout << "enter number";
+        //     cin >> sha[x];
+        //     cout << sha[x]<<endl;
+        //     x++;
+          
+        //   }
+          return 0;
+      }
