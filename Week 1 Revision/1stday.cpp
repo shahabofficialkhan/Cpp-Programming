5aaade3ef3de830c16230bf4f4e09f9ba8247054
Muhmmad Shahab <shahabofficialkhan@gmail.com>
2025-12-11 22:37:54 +0500

    Week 1 Revision

---
+++ Week 1 Revision/1stday.cpp
@@ -0,0 +1,19 @@
+//simple program
+#include <iostream>
+using namespace std;
+//In this rpogram i learn that how to print s imple program in c++
+//using cout and endl
+//cout is used to print the output on the screen
+int main(){ cout <<"Starting the name of Allah"<<endl;
+          cout <<"Me Shahab"<<endl;
+//int first me made mistake i did not out cout
+//how to comment in c++ using // and /* */ for multiple line comments
+//endl  and \n is used to print the output on the next line
+/*also i learn to how to save and run the program
+Logical error :the error which is not detect by comipler and exist in your logic eg:instead a+b you did a-b
+syntax error : the error is detect  by comipler did not run the program when error is exist in c++ conventional structure
+eg: you miss ; or intmain or << ,(){} # iotream etc */
+// i am also learn how to overload the things in C++ eg; x=5
+//x=x*x and x=x*x+1
+  return 0;
+}
---
+++ Week 1 Revision/2ndday.cpp
@@ -0,0 +1,14 @@
+//how to store a varible in a memory location
+#include <iostream>
+using namespace std;          
+int main (){ cout << "i am storing two varible " << endl;
+int a = 10;
+int x=a+20;
+int sum = x+a;
+cout << "the sum of two is " << sum << endl;
+
+
+
+
+          return 0;
+}
